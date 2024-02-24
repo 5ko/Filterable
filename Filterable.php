2d9aa14045f3766da1a1714c3f215f45777ba54a
Petko Yotov <5ko@5ko.fr>
2024-02-24 12:54:55 +0100

    Up version.

--- Filterable.php
+++ Filterable.php
@@ -7,7 +7,7 @@
   This extension creates search boxes for large lists and tables.
 */
 
-$RecipeInfo['Filterable']['Version'] = '2024-02-20';
+$RecipeInfo['Filterable']['Version'] = '2024-02-24';
 
 SDVA($Filterable, [
   'selector' => 'ul.filterable, ol.filterable, table.filterable',
