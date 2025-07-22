d85ccf996618a2f24a3809fc0e5bfe840a40824e
Petko Yotov <278737+5ko@users.noreply.github.com>
2025-07-22 09:19:57 +0200

    Refactor, expose Jets.makeOneFilterable.

--- Filterable.php
+++ Filterable.php
@@ -40,6 +40,3 @@ function initFilterable() {
 }
 
 initFilterable();
-
-
-
--- filterable.js
+++ filterable.js
@@ -41,65 +41,72 @@
     }
     
     for(var i=0; i<lists.length; i++) {
-      let list = lists[i];
-      
-      if(list.dataset.jets) continue;
-      var items = (list.tagName == 'TABLE') 
-        ? list.querySelectorAll('tr')
-        : list.children;
-        
-      if(list.tagName == 'TABLE') {
-        var thead = list.querySelector('thead');
-        if(!thead) {
-          thead = document.createElement('thead');
-          list.insertBefore(thead, list.firstChild);
-          thead.appendChild(items[0]);
-          items = list.querySelectorAll('tbody > tr');
-        }
-      }
-      
-      var msize = list.dataset.minsize? parseInt(list.dataset.minsize) : minsize;
-      // 1-4 rows/items : no filtering
-      if(items.length<msize) continue;
-      
-      if(list.tagName == 'OL') {
-        for(var j=0; j<items.length; j++) {
-          if(!items[j].getAttribute('value'))
-            items[j].setAttribute('value', j+1);
-        }
-      }
-      
-      makeFilterableCnt++;
-      
-      var placeholder = (list.tagName == 'TABLE')? conf.ftable : conf.flist;
-      if(list.dataset.inputfilter) {
-        var searchqs = list.dataset.inputfilter;
-      }
-      else {
-        var searchqs = '[data-jets="input'+makeFilterableCnt+'"]'
-        var input = '<input type="search" class="inputbox noprint" placeholder="'+placeholder+'" size="30" data-jets="input'+makeFilterableCnt+'"/>'
-        list.insertAdjacentHTML('beforebegin', input);
-      }
-      list.dataset.jets = 'content'+makeFilterableCnt;
-      
-      var tbody = (list.tagName == 'TABLE')? " tbody": '';
-      var options = {
-        searchTag: searchqs,
-        contentTag: '[data-jets="content'+makeFilterableCnt+'"]'+tbody
-      };
-      if(list.dataset.filtercols) {
-        var cols = list.dataset.filtercols.split(/[^\d]+/g);
-        var optcols = [];
-        for(var ii=0; ii<cols.length; ii++) {
-          var j = parseInt(cols[ii].trim());
-          if(!isNaN(j) && j>=0) optcols.push(j);
-        }
-        if(optcols.length) options.columns = optcols;
-      }
-      var x = new Jets(options);
-      list.style.marginTop = '.5em';
+      makeOneFilterable(lists[i]);
     }
   }
+  
+  function makeOneFilterable(list) {
+  
+    if(list.dataset.jets) return;
+    var items = (list.tagName == 'TABLE') 
+      ? list.querySelectorAll('tr')
+      : list.children;
+      
+    if(list.tagName == 'TABLE') {
+      var thead = list.querySelector('thead');
+      if(!thead) {
+        thead = document.createElement('thead');
+        list.insertBefore(thead, list.firstChild);
+        thead.appendChild(items[0]);
+        items = list.querySelectorAll('tbody > tr');
+      }
+    }
+    
+    var msize = list.dataset.minsize? parseInt(list.dataset.minsize) : minsize;
+    // 1-4 rows/items : no filtering
+    if(items.length<msize) return;
+    
+    if(list.tagName == 'OL') {
+      for(var j=0; j<items.length; j++) {
+        if(!items[j].getAttribute('value'))
+          items[j].setAttribute('value', j+1);
+      }
+    }
+    
+    makeFilterableCnt++;
+    
+    var placeholder = (list.tagName == 'TABLE')? conf.ftable : conf.flist;
+    if(list.dataset.inputfilter) {
+      var searchqs = list.dataset.inputfilter;
+    }
+    else {
+      var searchqs = '[data-jets="input'+makeFilterableCnt+'"]'
+      var input = '<input type="search" class="inputbox noprint" placeholder="'+placeholder+'" size="30" data-jets="input'+makeFilterableCnt+'"/>'
+      list.insertAdjacentHTML('beforebegin', input);
+    }
+    list.dataset.jets = 'content'+makeFilterableCnt;
+    
+    var tbody = (list.tagName == 'TABLE')? " tbody": '';
+    var options = {
+      searchTag: searchqs,
+      contentTag: '[data-jets="content'+makeFilterableCnt+'"]'+tbody
+    };
+    if(list.dataset.filtercols) {
+      var cols = list.dataset.filtercols.split(/[^\d]+/g);
+      var optcols = [];
+      for(var ii=0; ii<cols.length; ii++) {
+        var j = parseInt(cols[ii].trim());
+        if(!isNaN(j) && j>=0) optcols.push(j);
+      }
+      if(optcols.length) options.columns = optcols;
+    }
+    var x = new Jets(options);
+    list.style.marginTop = '.5em';
+  
+    
+  }
+  
+  Jets.makeOneFilterable = makeOneFilterable;
   
   document.addEventListener('DOMContentLoaded', makeFilterable);
 })(document.currentScript);
